e5181ca91c8a9bf401007b3ea75a90fea2d1d457
Landrey Smith <smithlandrey@gmail.com>
2026-02-22 14:49:36 -0800

    chore: Create solution and projects #1

---
+++ src/WorkDocsClone.Contracts/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace WorkDocsClone.Contracts;
+
+public class Class1
+{
+
+}
---
+++ tests/WorkDocsClone.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace WorkDocsClone.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
